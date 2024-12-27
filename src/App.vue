d39de1211e881a79d7cd61f14cba04b4d3ac05ce
tommiecc <teccleston77@gmail.com>
2024-12-27 13:44:42 +1000

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,32 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav class="navbar rounded-box shadow">
+    <div class="w-full md:flex md:items-center md:gap-2">
+      <div class="flex items-center justify-between">
+        <div class="navbar-start items-center justify-between max-md:w-full">
+          <a href="#" aria-label="Homepage Link">
+            <img src="/img/logo.jpg" alt="AAC Logo">
+          </a>
+          <div class="md:hidden">
+            <button type="button" class="collapse-toggle btn btn-outline btn-secondary btn-sm btn-square" data-collapse="#logo-navbar-collapse" aria-controls="logo-navbar-collapse" aria-label="Toggle navigation" >
+              <span class="icon-[tabler--menu-2] collapse-open:hidden size-4"></span>
+              <span class="icon-[tabler--x] collapse-open:block hidden size-4"></span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div id="logo-navbar-collapse" class="md:navbar-end collapse hidden grow basis-full overflow-hidden transition-[height] duration-300 max-md:w-full" >
+        <ul class="menu md:menu-horizontal gap-2 p-0 text-base max-md:mt-2">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+      </div>
     </div>
-  </header>
+  </nav>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundComponent from '../components/NotFoundComponent.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/:pathMatch(.*)', 
+      component: NotFoundComponent
+    },
     {
       path: '/',
       name: 'home',
